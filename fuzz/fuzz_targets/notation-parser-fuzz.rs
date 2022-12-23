2570d56980178e9fef6746e85b40679ae26284d0
ysthakur <2004yst@gmail.com>
2022-12-23 12:56:51 -0500

    Make notation-parser-fuzz use Arbitrary

--- fuzz/fuzz_targets/notation-parser-fuzz.rs
+++ fuzz/fuzz_targets/notation-parser-fuzz.rs
@@ -1,20 +1,20 @@
 #![no_main]
-use libfuzzer_sys::fuzz_target;
-use notation_dsl::helper::{parse_get_tab, parse_tab};
-use std::str;
 
-fuzz_target!(|data: &[u8]| {
-    if data.len() > 0 {
-        let opt = data[0] & 2;
-        match str::from_utf8(&data[1..]) {
-            Ok(in_string) => {
-                if opt == 0 {
-                    parse_tab(in_string);
-                } else if opt == 1 {
-                    parse_get_tab(in_string);
-                }
-            }
-            Err(..) => (),
-        }
+use arbitrary::Arbitrary;
+use libfuzzer_sys::fuzz_target;
+
+use notation_dsl::helper::{parse_get_tab, parse_tab};
+
+#[derive(Arbitrary, Debug)]
+struct FuzzInput {
+    choice: bool,
+    str: String,
+}
+
+fuzz_target!(|data: FuzzInput| {
+    if data.choice {
+        let _ = parse_tab(&data.str);
+    } else {
+        let _ = parse_get_tab(&data.str);
     }
 });
