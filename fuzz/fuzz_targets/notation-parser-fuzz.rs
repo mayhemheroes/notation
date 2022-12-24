ae3d516ddfade922384a7dca8959682f109f5c23
ysthakur <2004yst@gmail.com>
2022-12-23 19:19:33 -0500

    Only fuzz parse_tab

--- fuzz/fuzz_targets/notation-parser-fuzz.rs
+++ fuzz/fuzz_targets/notation-parser-fuzz.rs
@@ -3,18 +3,6 @@
 use arbitrary::Arbitrary;
 use libfuzzer_sys::fuzz_target;
 
-use notation_dsl::helper::{parse_get_tab, parse_tab};
-
-#[derive(Arbitrary, Debug)]
-struct FuzzInput {
-    choice: bool,
-    str: String,
-}
-
-fuzz_target!(|data: FuzzInput| {
-    if data.choice {
-        let _ = parse_tab(&data.str);
-    } else {
-        let _ = parse_get_tab(&data.str);
-    }
+fuzz_target!(|data: &str| {
+    notation_dsl::helper::parse_tab(data);
 });
