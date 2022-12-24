f00b3cd7d63ab247938dcd9267732290acf887bd
ysthakur <2004yst@gmail.com>
2022-12-23 19:33:21 -0500

    Don't depend on arbitrary

--- fuzz/fuzz_targets/notation-parser-fuzz.rs
+++ fuzz/fuzz_targets/notation-parser-fuzz.rs
@@ -1,8 +1,7 @@
 #![no_main]
 
-use arbitrary::Arbitrary;
 use libfuzzer_sys::fuzz_target;
 
 fuzz_target!(|data: &str| {
-    notation_dsl::helper::parse_tab(data);
+    let _ = notation_dsl::helper::parse_tab(data);
 });
